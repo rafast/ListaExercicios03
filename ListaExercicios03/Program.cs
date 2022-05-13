a69379f547d81375da782a47ba076fdcef441d8e
rafast <rafast.telles@gmail.com>
2022-05-13 16:32:08 -0300

    * .gitignore: Commit inicial
    * ListaExercicios03.sln:
    * Program.cs:
    * Unidade 1 - Exercicios - Parte 3.pdf:
    * ListaExercicios03.csproj:

---
+++ ListaExercicios03/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ListaExercicios03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

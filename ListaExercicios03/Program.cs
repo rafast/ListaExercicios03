af1fad79f15bd75fefdcc00b11b84e70e1136b6a
rafast <rafast.telles@gmail.com>
2022-05-14 09:15:40 -0300

    * ListaExercicios03.sln: * Questao03.csproj: * MetodoExtensao.cs:
    * Program.cs: Commitando solução da Questao03

--- ListaExercicios03/Program.cs
+++ ListaExercicios03/Program.cs
@@ -1,12 +1,21 @@
 ﻿using System;
 
-namespace ListaExercicios03
+namespace Questao03
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Testando a Questao 03");
+            Console.WriteLine("Imprimindo todos os números de Armstrong no intervalo de 1 a 10.000");
+            for (int i = 1; i <= 10000; i++)
+            {
+                if (i.IsArmstrong())
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
         }
     }
 }

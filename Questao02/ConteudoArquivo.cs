7b74a00835d442eaae0f2184e09975fe838f7296
rafast <rafast.telles@gmail.com>
2022-05-15 11:17:16 -0300

    * texto.txt: * Palavra.cs: * ignore.txt: * ConteudoArquivo.cs: * NovoIndiceRemissivo.cs:
    * ListaExercicios03.sln: Commitando solucao temporaria da questao 02

---
+++ Questao02/ConteudoArquivo.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Questao02
+{
+    public class ConteudoArquivo
+    {
+        public List<Palavra> ListaPalavras { get; set; }
+
+        public ConteudoArquivo()
+        {
+            ListaPalavras = new List<Palavra>();
+        }
+
+        public void Adiciona(Palavra palavra, int indice)
+        {
+            var palavraOuDefault = ListaPalavras.Where(p => p.Nome == palavra.Nome).SingleOrDefault();
+
+            if (palavraOuDefault == null)
+            {
+                ListaPalavras.Add(palavra);
+            }
+            else
+            {
+                palavraOuDefault.AddIndice(indice);
+            }
+        }
+
+        public void Imprime()
+        {
+            foreach (var palavra in ListaPalavras.OrderBy(p => p.Nome))
+            {
+                Console.Write($"{palavra.Nome} ({palavra.IndicesLinha.Count()}) ");
+                foreach (var linha in palavra.IndicesLinha.Distinct())
+                {
+                    Console.Write($"{linha + 1}, ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+    }
+}
---
+++ Questao02/Palavra.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Questao02
+{
+    public class Palavra
+    {
+        public string Nome { get; set; }
+        public List<int> IndicesLinha { get; set; }
+
+        public Palavra(string nome, int primeiroIndice)
+        {
+            Nome = nome.ToUpper();
+            IndicesLinha = new List<int>() { primeiroIndice };
+        }
+
+        public void AddIndice(int i)
+        {
+            IndicesLinha.Add(i);
+        }
+    }
+}
